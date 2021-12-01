96a9377b574bf8de8b137aab76c792fb74dcca63
daniel <dansku110@gmail.com>
2021-12-01 19:04:46 +0200

    Fixed the node addition. Changed it to async await. id types changed to string just in case

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Graph } from './components/Graph';
-import { Elements, addEdge, removeElements, Edge, Connection, isNode, isEdge, FlowElement } from 'react-flow-renderer';
+import { Elements, addEdge, removeElements, Edge, Node,Connection, isNode, isEdge, FlowElement } from 'react-flow-renderer';
 import * as nodeService from './services/nodeService'
 import * as edgeService from "./services/edgeService"
 import * as t from './types'
 
-const App : React.FC = () => {
+const App: React.FC = () => {
 
-	const [ nodeText, setNodeText ] = useState('')
-	const [ elements, setElements ] = useState<Elements>([])
+	const [nodeText, setNodeText] = useState('')
+	const [elements, setElements] = useState<Elements>([])
 
 	interface FlowInstance {
 		fitView: () => void;
@@ -25,60 +25,45 @@ const App : React.FC = () => {
 					{
 						id: String(n.id),
 						data: { label: n.description },
-						position: {x: n.x, y: n.y}
+						position: { x: n.x, y: n.y }
 					}
 				))
-	
+
 				const edgeElements: Elements = edges.map(e => (
 					{
-						id: String(e.source_id) + "-" + String(e.target_id), 
-						source: String(e.source_id), 
+						id: String(e.source_id) + "-" + String(e.target_id),
+						source: String(e.source_id),
 						target: String(e.target_id)
 					}
 				))
-				
+
 				setElements(nodeElements.concat(edgeElements));
 			});
 		});
 	};
-	useEffect(getElementsHook, [nodeText])
+	useEffect(getElementsHook, [])
 
 	/**
 	 * Creates a new node and stores it in the 'elements' React state. Nodes are stored in the database. 
 	 */
-	const createNode = (): void => {
+	const createNode = async(): Promise<void> => {
 		const n: t.INode = {
 			status: "ToDo",
 			description: nodeText,
 			priority: "Urgent",
 			x: 5 + elements.length * 10,
-			y: 5 + elements.length * 10 
+			y: 5 + elements.length * 10
 		}
-		
-		nodeService.sendNode(n)
-			.then( returnId => {
-				if(returnId){
-					console.log("Returned id:", returnId)
-					console.log("Adding node:", {
-						id: returnId,
-						data: { label: nodeText },
-						position: { x: 5 + elements.length * 10, y: 5 + elements.length * 10 }
-					})
-					setElements(elements.concat({
-						id: returnId,
-						data: { label: nodeText },
-						position: { x: 5 + elements.length * 10, y: 5 + elements.length * 10 }
-					}))
-				} else {
-					console.log("No returnId returned")
-				}
-					
-			})
-			.catch ( (e: Error) => {
-				console.log("Failed to add node in backend: ")
-				console.log(e)
-			})
-			
+		const returnId: string | undefined = await nodeService.sendNode(n)
+		if(returnId){
+			const b: Node = {
+				id: String(returnId),
+				data: { label: nodeText},
+				position: {x: 5 + elements.length * 10, y: 5 + elements.length * 10}
+			}
+			console.log("WHat is the node?", b)
+			setElements(elements.concat(b))
+		}
 		setNodeText('')
 	}
 
@@ -86,11 +71,11 @@ const App : React.FC = () => {
 	//eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const onConnect = (params: Edge<any> | Connection) => {
 		if (params.source && params.target) {
-			setElements( (els) => addEdge(params, els) )
-			
-			edgeService.sendEdge({ 
-				source_id: Number(params.source), 
-				target_id: Number(params.target)
+			setElements((els) => addEdge(params, els))
+
+			edgeService.sendEdge({
+				source_id: params.source,
+				target_id: params.target
 			});
 		} else {
 			console.log("source or target of edge is null, unable to send to db");
@@ -102,13 +87,13 @@ const App : React.FC = () => {
 	 * onElementsRemove.
 	 */
 	const compareElementsEdgesFirst = (a: FlowElement, b: FlowElement): number => {
-		if(isNode(a)){
-			if(isNode(b))
+		if (isNode(a)) {
+			if (isNode(b))
 				return 0
 			else
 				return 1
 		} else {	// a is an Edge
-			if(isNode(b))
+			if (isNode(b))
 				return -1
 			else
 				return 0
@@ -121,13 +106,13 @@ const App : React.FC = () => {
 	 */
 	const onElementsRemove = async (elementsToRemove: Elements) => {
 		// Must remove edges first to prevent referencing issues in database
-		const sortedElementsToRemove = elementsToRemove.sort( compareElementsEdgesFirst )
-		for ( const e of sortedElementsToRemove ) {
-			if(isNode(e)){
+		const sortedElementsToRemove = elementsToRemove.sort(compareElementsEdgesFirst)
+		for (const e of sortedElementsToRemove) {
+			if (isNode(e)) {
 				await nodeService.deleteNode(e)
 			}
-			else if(isEdge(e)) {
-				await edgeService.deleteEdge(e).catch( (e: Error) => console.log("Error when deleting edge", e) )
+			else if (isEdge(e)) {
+				await edgeService.deleteEdge(e).catch((e: Error) => console.log("Error when deleting edge", e))
 			}
 		}
 
@@ -137,17 +122,17 @@ const App : React.FC = () => {
 	const onLoad = (reactFlowInstance: FlowInstance) => reactFlowInstance.fitView();
 
 	return (
-		<div>		
+		<div>
 			<h2>Tasks</h2>
 			<div>
 				<h3>Add task</h3>
 				<div>
-					Text: <input id='nodetext' type='text' value={nodeText} onChange={ ({ target }) => setNodeText(target.value)}/>
+					Text: <input id='nodetext' type='text' value={nodeText} onChange={({ target }) => setNodeText(target.value)} />
 					<button onClick={createNode}>Add</button>
 				</div>
 			</div>
 			<div className="graph">
-				<Graph 
+				<Graph
 					elements={elements}
 					onConnect={onConnect}
 					onElementsRemove={onElementsRemove}
@@ -160,4 +145,4 @@ const App : React.FC = () => {
 	);
 }
 
-export default App; 
+export default App;
--- src/client/src/services/nodeService.ts
+++ src/client/src/services/nodeService.ts
@@ -6,7 +6,6 @@ export const baseUrl = "/api/node"
 // Should be possible to give "getAll" a return type
 const getAll  = async (): Promise<INode[]> => {
   const node = await axios.get<INode[]>(baseUrl)
-  console.log(node.data)
   return node.data
 }
 
@@ -14,7 +13,6 @@ const getAll  = async (): Promise<INode[]> => {
 //then return an object
 const sendNode = async(node: INode): Promise<string> => {
   const response = await axios.post(baseUrl, node)
-  //console.log("Node added", response.data)
   //Return the id for that element created by the database
   return response.data.rows[0].id
 }
