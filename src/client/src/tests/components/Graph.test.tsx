fe988bd5f4fb73ac0353c6947403e84bc81bf206
Tenho Korhonen <tenho.korhonen@aalto.fi>
2021-12-11 22:30:29 +0200

    copy tests from front-tests

--- src/client/src/tests/components/Graph.test.tsx
+++ src/client/src/tests/components/Graph.test.tsx
@@ -2,39 +2,96 @@
  * @jest-environment jsdom
  */
 
-import React from 'react'
-import { render } from '@testing-library/react'
-
-import { Graph } from '../../components/Graph'
-import { ReactFlowProps } from 'react-flow-renderer'
-
-const renderGraph = () => {
-    const props: ReactFlowProps = {
-        elements: []
-    };
-    return render(<Graph { ...props } />);
-}
-
-describe('<Graph>', () => {
-    test('graph is visible for users', () => {
-        
-        const component = renderGraph();
-        expect(component).toBeVisible
-            
-    
-    })
-
-    test('Graph contains a mini map', () => {
-        const component = renderGraph();
-        const miniMap = component.container.querySelector('MiniMap');
-        
-        expect(miniMap).toBeInTheDocument;
-        expect(miniMap).toBeVisible;
-    })
-})
-
-    
-
-
-
-
+ import React from 'react'
+ import { render, RenderResult } from '@testing-library/react'
+ 
+ import { Graph } from '../../components/Graph'
+ import { Elements } from 'react-flow-renderer'
+ import '@testing-library/jest-dom';
+ import "@testing-library/jest-dom/extend-expect";
+ 
+ const renderGraph = (elements: Elements) => {
+     return render(
+         <Graph
+             elements={elements} 
+             onLoad={ (reactFlowInstance) => reactFlowInstance.fitView() }
+         />
+     );
+ }
+ 
+ describe('<Graph>', () => {
+     const testElements = ([
+         {
+             id: '1',
+             type: 'input',
+             data: { label: 'Input node' },
+             position: { x: 100, y: 5 },
+         },
+         {
+             id: '2',
+             type: 'default',
+             data: { label: 'Default node' },
+             position: { x: 100, y: 100 },
+         },
+         {
+             id: '3',
+             type: 'output',
+             data: { label: 'Output node' },
+             position: { x: 100, y: 200 },
+         },
+         {
+             id: 'e1-2',
+             source: '1',
+             target: '2'
+         },
+         {
+             id: 'e2-3',
+             source: '2',
+             target: '3'
+         }
+     ])
+ 
+     let testGraph: RenderResult
+ 
+     beforeEach(() => {
+         testGraph = renderGraph(testElements)
+ 
+         // This fixes displaying edges in tests
+         Object.defineProperties(window.HTMLElement.prototype, {
+             offsetHeight: {
+                 get() {
+                     return parseFloat(this.style.height) || 1;
+                 },
+             },
+             offsetWidth: {
+                 get() {
+                     return parseFloat(this.style.width) || 1;
+                 },
+             },
+         });
+ 
+     })
+         
+ 
+     test('is visible for users', () => {
+         expect(testGraph).toBeVisible
+     })
+ 
+     test('displays nodes', () => {
+         const c = testGraph.container.querySelector('.react-flow')
+         expect(c).toHaveTextContent('Input node')
+         expect(c).toHaveTextContent('Output node')
+         expect(c).toHaveTextContent('Default node')
+     })
+ 
+     test('displays the right amount of nodes', () => {
+         const c = testGraph.container.querySelectorAll('.react-flow__node')
+         expect(c).toHaveLength(3)
+     })
+ 
+     test('displays the right amount of edges', () => {
+         const c = testGraph.container.querySelectorAll('.react-flow__edge')
+         expect(c).toHaveLength(2)
+     })
+ })
+ 
