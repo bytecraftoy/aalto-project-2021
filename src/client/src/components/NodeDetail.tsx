940a1daa008aefef27c3c7b07ffd9ebd87a3df77
Shun Kito <shun.kito@aalto.fi>
2022-04-10 21:45:12 +0300

    Now shows ids on top of the sidebar, also changed ui of edge's sidebar

--- src/client/src/components/NodeDetail.tsx
+++ src/client/src/components/NodeDetail.tsx
@@ -5,7 +5,10 @@ import { AssignedUsers } from './AssignedUsers';
 import { AssignUsers } from './AssignUsers';
 import { NodeForm } from './NodeForm';
 import './styles/Sidebar.css';
-import { BsClipboardCheck, BsExclamationCircle, BsHash } from 'react-icons/bs';
+import {
+    BsClipboardCheck,
+    BsExclamationCircle /* BsHash */,
+} from 'react-icons/bs';
 
 interface NodeDetailProps {
     element: Node<INode>;
@@ -51,10 +54,10 @@ export const NodeDetail = (props: NodeDetailProps): JSX.Element => {
                     <b className="title">Priority: </b>
                     {data.priority}
                 </p>
-                <p>
+                {/* <p>
                     <BsHash className="icon" /> <b className="title">ID: </b>
                     {data.id}
-                </p>
+                </p> */}
                 <AssignedUsers node={data} />
             </>
         );
@@ -62,7 +65,7 @@ export const NodeDetail = (props: NodeDetailProps): JSX.Element => {
 
     return (
         <>
-            <h5>Node:</h5>
+            <h5>#{data.id}</h5>
             {content}
         </>
     );
