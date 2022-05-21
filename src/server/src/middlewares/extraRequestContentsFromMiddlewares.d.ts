933c62e3880f13e3e997b925c5f4c94c14914586
Jaakko Hannikainen <jgke@jgke.fi>
2022-05-21 11:23:20 +0300

    Remove a bunch of dead content

--- src/server/src/middlewares/extraRequestContentsFromMiddlewares.d.ts
+++ src/server/src/middlewares/extraRequestContentsFromMiddlewares.d.ts
@@ -3,6 +3,8 @@ import winston from 'winston';
 declare global {
     declare namespace Express {
         export interface Request {
+            token?: string;
+            user?: { email: string; id: number; iat: number };
             logger: winston.Logger;
         }
     }
